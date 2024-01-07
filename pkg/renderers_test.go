28a5aacd96d55c4fedf822d12f264ab6a9b5fe1f
Ralph Caraveo <deckarep@gmail.com>
2024-01-06 19:02:52 -0800

    Adding TestRenderIPs unit-test

--- pkg/renderers_test.go
+++ pkg/renderers_test.go
@@ -24,3 +24,27 @@ func TestRenderRemoteSummary(t *testing.T) {
 
 	assert.Equal(t, b.String(), "Finished: successes: 0, failures: 3, elapsed (secs): 0.78\n")
 }
+
+func TestRenderIPs(t *testing.T) {
+	var b bytes.Buffer
+	ctx := context.Background()
+	cfgCtx := NewConfigCtx()
+	cfgCtx.IPsDelimiter = "\n"
+	ctx = context.WithValue(ctx, CtxKeyConfig, cfgCtx)
+
+	tv := &GeneralTableView{
+		ContextView: ContextView{},
+		TailnetView: TailnetView{},
+		SelfView:    SelfView{},
+		Rows: [][]string{
+			{"foo", "bar", "127.0.0.1"},
+			{"coo", "car", "127.0.0.2"},
+			{"soo", "sar", "127.0.0.3"},
+			{"too", "tar", "127.0.0.4"},
+		},
+	}
+
+	err := RenderIPs(ctx, tv, &b)
+	assert.NoError(t, err, "RenderIPs should have returned no error")
+	assert.Equal(t, b.String(), "127.0.0.1\n127.0.0.2\n127.0.0.3\n127.0.0.4\n")
+}
