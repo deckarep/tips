984b7b23855111cb7288a1d85d69207c6847baa4
Ralph Caraveo <deckarep@gmail.com>
2024-01-12 10:49:49 -0800

    Adding a more comprehensive ParseFilter function which handles the cli parsing

--- pkg/filters_test.go
+++ pkg/filters_test.go
@@ -7,13 +7,19 @@ import (
 )
 
 func TestParseFilter(t *testing.T) {
-	parsedResult, err := ParseFilter("peanut, walnut, pecan")
+	// Note: much more robust testing of the parser occurs in the filtercomp package.
+	// It makes this testing somewhat redundant.
+
+	// An filter expression with imbalanced parenthesis should return an error.
+	ast, err := ParseFilter("(hello, world")
+	assert.Error(t, err, "imbalanced parenthesis detected")
+
+	// An empty filter should return no error with a nil ast.
+	ast, err = ParseFilter("")
 	assert.NoError(t, err)
-	assert.NotNil(t, parsedResult)
+	assert.Nil(t, ast)
 
-	if parsedResult == nil {
-		t.Error("expected populated filters but got nil")
-	}
-
-	// TODO: more robust testing here.
+	// An empty filter should return no error with a nil ast.
+	ast, err = ParseFilter("how are you doing?")
+	assert.Error(t, err, "parser did not run to completion, tokens were not fully consumed")
 }
