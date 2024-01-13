96b3ab0795488e0b3f8f72263cb82e7ec4120f12
Ralph Caraveo <deckarep@gmail.com>
2024-01-12 16:43:16 -0800

    Adding executeFilters unit test

--- pkg/filters_test.go
+++ pkg/filters_test.go
@@ -1,7 +1,12 @@
 package pkg
 
 import (
+	"context"
 	"testing"
+
+	"github.com/deckarep/tips/pkg/tailscale_cli"
+
+	"github.com/tailscale/tailscale-client-go/tailscale"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -22,4 +27,99 @@ func TestParseFilter(t *testing.T) {
 	// An empty filter should return no error with a nil ast.
 	_, err = ParseFilter("how are you doing?")
 	assert.Error(t, err, "parser did not run to completion, tokens were not fully consumed")
+}
+
+func TestExecuteFilters(t *testing.T) {
+	ctx := context.Background()
+	cfg := NewConfigCtx()
+
+	ast, err := ParseFilter("user@gmail.com")
+	assert.NoError(t, err)
+
+	cfg.Filters = ast
+
+	ctx = context.WithValue(ctx, CtxKeyConfig, cfg)
+
+	devs := []*WrappedDevice{
+		{Device: tailscale.Device{
+			User:          "user@gmail.com",
+			Tags:          []string{"foo", "bar", "baz"},
+			Addresses:     []string{"127.0.0.1"},
+			OS:            "rasbarbarian",
+			ClientVersion: "1.23.45-deadbeef",
+		}},
+		{Device: tailscale.Device{
+			User:          "user@gmail.com",
+			Tags:          []string{"foo", "biz", "bang"},
+			Addresses:     []string{"127.0.0.2"},
+			OS:            "loonix",
+			ClientVersion: "1.23.46-deadbeef",
+		}},
+		{Device: tailscale.Device{
+			User:          "user@gmail.com",
+			Tags:          []string{"poo", "par", "paz"},
+			Addresses:     []string{"127.0.0.3"},
+			OS:            "windoze",
+			ClientVersion: "1.23.2-deadbeef",
+		}},
+		{Device: tailscale.Device{
+			User:          "user@gmail.com",
+			Tags:          []string{"voo", "foo", "var", "vaz"},
+			Addresses:     []string{"127.0.0.4"},
+			OS:            "bigmacos",
+			ClientVersion: "1.23.1-deadbeef",
+		}, EnrichedInfo: &tailscale_cli.DeviceInfo{
+			HasExitNodeOption: true,
+		}},
+	}
+
+	// Apply Users filter.
+	filteredResults := executeFilters(ctx, devs)
+	assert.NotNil(t, filteredResults)
+	assert.Equal(t, len(filteredResults), 4)
+
+	// Apply Tags filter.
+	ast, err = ParseFilter("foo")
+	assert.NoError(t, err)
+	cfg.Filters = ast
+
+	filteredResults = executeFilters(ctx, devs)
+	assert.NotNil(t, filteredResults)
+	assert.Equal(t, len(filteredResults), 3)
+
+	// Apply Tags filter.
+	ast, err = ParseFilter("127.0.0.4")
+	assert.NoError(t, err)
+	cfg.Filters = ast
+
+	filteredResults = executeFilters(ctx, devs)
+	assert.NotNil(t, filteredResults)
+	assert.Equal(t, len(filteredResults), 1)
+
+	// Apply OS filter.
+	ast, err = ParseFilter("(windoze | loonix)")
+	assert.NoError(t, err)
+	cfg.Filters = ast
+
+	filteredResults = executeFilters(ctx, devs)
+	assert.NotNil(t, filteredResults)
+	assert.Equal(t, len(filteredResults), 2)
+
+	// Apply Version filter.
+	ast, err = ParseFilter("1.23.4*")
+	assert.NoError(t, err)
+	cfg.Filters = ast
+
+	filteredResults = executeFilters(ctx, devs)
+	assert.NotNil(t, filteredResults)
+	assert.Equal(t, len(filteredResults), 2)
+
+	// Exit node filter.
+	ast, err = ParseFilter("+exit")
+	assert.NoError(t, err)
+	cfg.Filters = ast
+
+	filteredResults = executeFilters(ctx, devs)
+	assert.NotNil(t, filteredResults)
+	assert.Equal(t, len(filteredResults), 1)
 }
